d6e00c5cf6dfbe364be5bc0251c2205190eb2619
PAPPURAJ BHOTTACHARJEE <45383061+PAPPURAJ@users.noreply.github.com>
2022-08-28 15:32:50 +0600

    UCTC

--- bike_safety.ino
+++ bike_safety.ino
@@ -3,7 +3,6 @@
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 #include <TinyGPS++.h>
-#include<SoftwareSerial.h>
 #include "FirebaseESP8266.h"
 
 Adafruit_MPU6050 mpu;
@@ -14,14 +13,12 @@ FirebaseJson json;
 #define FIREBASE_HOST "bikesafety-870a7-default-rtdb.asia-southeast1.firebasedatabase.app"  
 #define FIREBASE_AUTH "EBuWwzzxfRXKcBtc571zhRykPT2r03XghhkQTYK0"
 
-#define WIFI_SSID "BdREN-DUET@QK"     
-#define WIFI_PASSWORD "Qk@DueT22" 
+#define WIFI_SSID "BikeSafety_IOT"     
+#define WIFI_PASSWORD "12345678" 
 
 
 
 double Lat=10,Lon=11,x,y,tem;
-
-SoftwareSerial myS(D5,D6);
 
 
 
@@ -36,7 +33,6 @@ void writeDB(String field,String value){
 
 
 void setup(void) {
-  myS.begin(9600);
   
   Serial.begin(9600);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -75,10 +71,9 @@ double xLimit=5.0,yLimit=5.0, tLimit=50.0;
 
 void loop() {
 
-
-     while(myS.available())             
+     while(Serial.available())             
   {
-    gps.encode(myS.read());       
+    gps.encode(Serial.read());       
    
   }
   if(gps.location.isUpdated())  
@@ -86,8 +81,9 @@ void loop() {
    
     Lat=gps.location.lat();
     Lon=gps.location.lng();
-    Serial.println(Lat,6);
-    Serial.println(Lon,6);
+    Serial.print("GPS: "+String(gps.location.lat(),6));
+    Serial.print("  ");
+    Serial.print(String(gps.location.lng(),6)+"   ");
 
     writeDB("/Location/Lat",String(Lat,6));
     writeDB("/Location/Lon",String(Lon,6));
@@ -117,7 +113,7 @@ void loop() {
    writeDB("/Y",String(y));
    writeDB("/Temp",String(tem));
    writeDB("/Danger",String(xLimit<=x || yLimit<=y || tLimit<=tem)); 
-  
+
 
   delay(250);
 
